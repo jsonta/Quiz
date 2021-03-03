3c8e8dfb7a117a3423f4b57e68b14459c96060e1
jsonta <56021506+jsonta@users.noreply.github.com>
2021-03-03 14:33:07 +0100

    Implemented mock login operation and mock (empty) main menu

--- quiz/src/main/java/github/jsonta/quiz/Login.java
+++ quiz/src/main/java/github/jsonta/quiz/Login.java
@@ -1,11 +1,47 @@
 package github.jsonta.quiz;
-import javax.swing.JOptionPane;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 
-public class Login extends Thread {
-    public Login(){}
+public class Login {
+    private String email, paswd, status;
+    private boolean loggedIn = false;
+    private LoginThread thread;
+    private final PropertyChangeSupport pcs;
     
-    @Override
-    public void run() {
-        JOptionPane.showMessageDialog(null, "unimplemented");
+    public Login() {
+        pcs = new PropertyChangeSupport(this);
+    }
+    
+    public void addPropertyChangeListener(PropertyChangeListener pcl) {
+        pcs.addPropertyChangeListener(pcl);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener pcl) {
+        pcs.removePropertyChangeListener(pcl);
+    }
+    
+    public void setLoginData(String e, String p) {
+        this.email = e;
+        this.paswd = p;
+    }
+    
+    public void executeLogin() {
+        thread = new LoginThread(email, paswd);
+        thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {}
+        
+        pcs.firePropertyChange("userLoggedIn", loggedIn, thread.getLoggedIn());
+        this.loggedIn = thread.getLoggedIn();
+        this.status = thread.getStatus();
+    }
+    
+    public boolean getLoggedIn() {
+        return loggedIn;
+    }
+    
+    public String getStatus() {
+        return status;
     }
 }
